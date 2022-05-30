cd87b8e4e8a4f86ff1df82b05f7275871b1adc3f
Varvara-Kravtsova <varvara.kravczova2002@gmail.com>
2022-05-30 15:09:56 +0300

    feat(app): add fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
+
+import { BookList } from "./components/BookList/BookList";
 import { AppRouter } from "./router/AppRouter";
+import { IBook } from "./types/index";
 
 const App = () => {
+ const [books, setBooks] = useState<IBook[]>([]);
+ useEffect(() => {
+  fetch("https://api.itbook.store/1.0/new")
+   .then((res) => res.json())
+   .then((data) => {
+    setBooks(data.books);
+   });
+ }, []);
  return (
   <BrowserRouter>
    <AppRouter />
+   <div>
+    <h1>Books</h1>
+    <BookList books={books} />
+   </div>
   </BrowserRouter>
  );
 };
