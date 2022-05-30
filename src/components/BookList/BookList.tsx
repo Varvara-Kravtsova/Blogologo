c5469c63d6c1e58d0544cf43cc284c92c1e7c49d
Varvara-Kravtsova <varvara.kravczova2002@gmail.com>
2022-05-30 15:11:24 +0300

    fix(booklist): fix bug in booklist

--- src/components/BookList/BookList.tsx
+++ src/components/BookList/BookList.tsx
@@ -2,27 +2,6 @@ import { IBook } from "../../types";
 import { BookItem } from "../BookItem/BookItem";
 import { StyledBookList } from "./styles";
 
-import { useEffect, useState } from "react";
-
-export const App = () => {
- const [books, setBooks] = useState<IBook[]>([]);
-
- useEffect(() => {
-  fetch("https://api.itbook.store/1.0/new")
-   .then((res) => res.json())
-   .then((data) => {
-    setBooks(data.books);
-   });
- }, []);
-
- return (
-  <div>
-   <h1>Books</h1>
-   <BookList books={books} />
-  </div>
- );
-};
-
 interface IBookListProps {
  books: IBook[];
 }
