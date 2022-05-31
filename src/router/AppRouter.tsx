f60f1df1eab1a2bedef39ecc1c949fe7ee3610e9
Varvara-Kravtsova <varvara.kravczova2002@gmail.com>
2022-05-31 04:39:58 +0300

    fix(approuter): fix imports

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "../pages/Home";
-import Login from "../pages/Login";
-import Register from "../pages/Register";
-import NotFound from "../pages/NotFound";
+import { Login } from "../pages/Login";
+import { Register } from "../pages/Register";
+import { NotFound } from "../pages/NotFound";
 import { routes } from "../routes/routes";
 
 export const AppRouter = () => {
