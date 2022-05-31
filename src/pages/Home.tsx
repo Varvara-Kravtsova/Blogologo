1cb81dc0d3e3af36b9b1144bbed845ae9a465802
Varvara-Kravtsova <varvara.kravczova2002@gmail.com>
2022-05-31 04:22:40 +0300

    feat(home): add useSelector

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,14 @@
+import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import { routes } from "../routes/routes";
+import { RootStore } from "../store/store";
 
 export const Home = () => {
- return (
-  <div>
-   <h1>Home</h1>
-   <Navigate to={routes.HOME} />
-  </div>
- );
+ const { isAuth, email } = useSelector(({ user }: RootStore) => user);
+
+ if (isAuth) {
+  return <div>{email}</div>;
+ }
+
+ return <Navigate to={routes.SIGN_UP} />;
 };
