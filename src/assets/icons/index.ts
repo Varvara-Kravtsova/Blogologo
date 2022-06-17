acec038a4dc7351a0460ec8bac20c036028b1eb4
Varvara-Kravtsova <varvara.kravczova2002@gmail.com>
2022-06-17 12:46:07 +0300

    feat(assets): add icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,7 +1,33 @@
 import { ReactComponent as Back } from "./back.svg";
 import { ReactComponent as LogoIcon } from "./logo.svg";
-import { ReactComponent as Cart } from "./cart.svg";
+import { ReactComponent as Cart } from "./shopping-bag.svg";
 import { ReactComponent as User } from "./user.svg";
 import { ReactComponent as Details } from "./details.svg";
+import { ReactComponent as ArrowLeft } from "./arrow-left.svg";
+import { ReactComponent as ArrowRight } from "./arrow-right.svg";
+import { ReactComponent as ArrowDown } from "./arrow-down.svg";
+import { ReactComponent as Facebook } from "./facebook.svg";
+import { ReactComponent as EmptyStar } from "./empty-star.svg";
+import { ReactComponent as FullStar } from "./full-star.svg";
+import { ReactComponent as Twitter } from "./twitter.svg";
+import { ReactComponent as Heart } from "./heart.svg";
+import { ReactComponent as Circle } from "./circle.svg";
+import { ReactComponent as Search } from "./search.svg";
 
-export { Back, LogoIcon, Cart, User, Details };
+export {
+ Back,
+ LogoIcon,
+ Cart,
+ User,
+ Details,
+ ArrowLeft,
+ ArrowRight,
+ ArrowDown,
+ Facebook,
+ EmptyStar,
+ FullStar,
+ Twitter,
+ Heart,
+ Circle,
+ Search,
+};
