a50b15638d8285cc976220dfdd019c738e1a305a
Varvara-Kravtsova <varvara.kravczova2002@gmail.com>
2022-06-17 13:10:12 +0300

    feat(pages): add page for search

---
+++ src/pages/SearchBooks.tsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { BookList } from "../components/BookList/BookList";
+import BackButton from "../components/BackButton/BackButton";
+import { Title } from "../components/Title/Title";
+import { Pagination } from "../components/Pagination/Pagination";
+import { useAppSelector, useAppDispatch } from "../store/hooks/hooks";
+import { getBooks } from "../store/selectors/bookSelectors";
+import { fetchBooksBySearch } from "../store/slices/bookSlice";
+
+export const SearchBooks = () => {
+ const { title = "", page = "" } = useParams();
+ const { books } = useAppSelector(getBooks);
+ const dispatch = useAppDispatch();
+
+ useEffect(() => {
+  dispatch(fetchBooksBySearch({ title, page }));
+ }, [title, dispatch, page]);
+
+ return (
+  <>
+   <BackButton />
+   <Title> '{title}'' search results</Title>
+   <BookList books={books ? books : []} />
+   <Pagination />
+  </>
+ );
+};
