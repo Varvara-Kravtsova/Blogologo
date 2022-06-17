2e3fb50387b19c08b6e5ebacc4fb848ae5500b0a
Varvara-Kravtsova <varvara.kravczova2002@gmail.com>
2022-06-17 13:16:36 +0300

    feat(index): add firebase

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,11 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { App } from "./App";
 import { store } from "./store/store";
+import "./firebase";
 
 const app = document.getElementById("root");
 if (!app) {
- throw new Error("No root element found");
+ throw new Error("Root element not found");
 }
 const root = createRoot(app);
 root.render(
