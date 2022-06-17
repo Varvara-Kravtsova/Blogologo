91d068b6f87d84477fb24ce8ec0573615c885123
Varvara-Kravtsova <varvara.kravczova2002@gmail.com>
2022-06-17 12:49:39 +0300

    feat(types): add some types

---
+++ src/types/index.ts
@@ -0,0 +1,122 @@
+export interface IBook {
+ image: string;
+ isbn13: string;
+ price: string;
+ subtitle: string;
+ title: string;
+ url: string;
+}
+
+export interface INewBooks {
+ books: IBook[];
+ error: any;
+ total: string;
+ status: RequestStatusType;
+}
+
+export interface INewBooksApi {
+ books: IBook[];
+ error: string;
+ total: string;
+ page: string;
+}
+
+export interface IBookDetailsApi {
+ authors: string;
+ desc: string;
+ error: string;
+ image: string;
+ isbn10: string;
+ isbn13: string;
+ language: string;
+ pages: string;
+ pdf: { [key: string]: string };
+ price: string;
+ publisher: string;
+ rating: string;
+ subtitle: string;
+ title: string;
+ url: string;
+ year: string;
+}
+
+export type RequestStatusType = "idle" | "loading" | "success" | "error";
+
+export interface IBookApi {
+ books: IBook[];
+ error: any;
+ status: RequestStatusType;
+ total: string;
+ result: IBookDetailsApi;
+}
+
+export interface IBooks {
+ response: boolean | null;
+ results: IBook[];
+ totalResults: number;
+ currentPage?: number;
+ totalPages: number;
+}
+
+export interface ISearchBooksApi extends INewBooksApi {
+ page: string;
+}
+
+export interface IUser {
+ sortMode: "asc" | "desc";
+ favorites: IBook[];
+ isAuth: boolean;
+ email: string | undefined;
+ name: string | undefined;
+ password: string | undefined;
+}
+
+export interface IFavorites {
+ favorites: IBook[];
+}
+
+export interface ICarts {
+ cards: IBookCartApi[];
+ total: number;
+ isLoading: boolean;
+}
+
+export interface ICart {
+ image: string;
+ title: string;
+ authors: string;
+ year: string;
+ price: string;
+ isbn13: string;
+ quantity: number;
+ totalPrice: string;
+}
+
+export interface ICartPage {
+ results: ICart[];
+ vat: number;
+ sumTotal: number;
+ total: number;
+}
+
+export interface ICartInfo {
+ image: string;
+ isbn13: string;
+ price: string;
+ subtitle: string;
+ title: string;
+ url: string;
+ year: string;
+ publisher: string;
+ rating: string;
+ authors: string;
+}
+
+export interface IBookCartApi extends IBookDetailsApi {
+ amount: number;
+}
+
+export interface IArguments {
+ title: string;
+ page: string;
+}
