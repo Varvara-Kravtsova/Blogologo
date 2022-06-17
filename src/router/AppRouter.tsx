179a72be619db53dfea50a92e00426f910ae657c
Varvara-Kravtsova <varvara.kravczova2002@gmail.com>
2022-06-17 15:33:32 +0300

    feat(router): add page for favorites

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import MainTemplate from "../components/MainTemplate/MainTemplate";
 import { DetailsBook } from "../pages/DetailsBook";
+import { FavoritesBooks } from "../pages/FavoritesBooks";
 import { Home } from "../pages/Home";
 import { SearchBooks } from "../pages/SearchBooks";
 import { routes } from "../routes/routes";
@@ -12,6 +13,7 @@ export const AppRouter = () => {
     <Route index element={<Home />} />
     <Route path={routes.DETAILS_BOOK} element={<DetailsBook />} />
     <Route path={routes.SEARCH} element={<SearchBooks />} />
+    <Route path={routes.FAVORITES} element={<FavoritesBooks />} />
    </Route>
   </Routes>
  );
