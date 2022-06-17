b2230a70dfd201e19d64bd784d43a550da3c4d6d
Varvara-Kravtsova <varvara.kravczova2002@gmail.com>
2022-06-17 13:12:01 +0300

    feat(store): add user and cart reducers

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import bookReducer from "./slice/bookSlice";
+import bookReducer from "./slices/bookSlice";
+import userReducer from "./slices/userSlice";
+import cartReducer from "./slices/cartSlice";
 
 export const store = configureStore({
  reducer: {
   books: bookReducer,
+  user: userReducer,
+  cart: cartReducer,
  },
 });
 export type RootState = ReturnType<typeof store.getState>;
