353ebb705561c30079c09558a44dbca8096437d7
Varvara-Kravtsova <varvara.kravczova2002@gmail.com>
2022-06-20 13:46:07 +0300

    feat(router): add account, signIn/Up

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,9 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import MainTemplate from "../components/MainTemplate/MainTemplate";
+import { Account } from "../pages/Account";
 import { DetailsBook } from "../pages/DetailsBook";
 import { FavoritesBooks } from "../pages/FavoritesBooks";
 import { Home } from "../pages/Home";
 import { SearchBooks } from "../pages/SearchBooks";
+import { SignIn } from "../pages/SignIn";
+import { SignUp } from "../pages/SignUp";
 import { routes } from "../routes/routes";
 
 export const AppRouter = () => {
@@ -14,6 +17,9 @@ export const AppRouter = () => {
     <Route path={routes.DETAILS_BOOK} element={<DetailsBook />} />
     <Route path={routes.SEARCH} element={<SearchBooks />} />
     <Route path={routes.FAVORITES} element={<FavoritesBooks />} />
+    <Route path={routes.SIGN_IN} element={<SignIn />} />
+    <Route path={routes.SIGN_UP} element={<SignUp />} />
+    <Route path={routes.ACCOUNT} element={<Account />} />
    </Route>
   </Routes>
  );
