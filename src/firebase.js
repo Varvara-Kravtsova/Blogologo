28f6351e7a7e767e8561467b4496a09e3c61d8a3
Varvara-Kravtsova <varvara.kravczova2002@gmail.com>
2022-06-20 12:36:20 +0300

    fix(firebase): modify firebase app

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
- apiKey: process.env.REACT_APP_API_KEY,
- authDomain: process.env.REACT_APP_AUTH_DOMAIN,
- projectId: process.env.REACT_APP_PROJECT_ID,
- storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
- messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
- appId: process.env.REACT_APP_APP_ID,
+ apiKey: "AIzaSyBqBeWbWGmiKWfmuq--s-19uyR-sGqSp6g",
+ authDomain: "bookstore-6e4f9.firebaseapp.com",
+ projectId: "bookstore-6e4f9",
+ storageBucket: "bookstore-6e4f9.appspot.com",
+ messagingSenderId: "456358342846",
+ appId: "1:456358342846:web:cc25c2a074e5db1b4aeeef",
 };
 
 // Initialize Firebase
