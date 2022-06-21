4b07af4ed517270c86f7f133c67e42ee24f2d7ed
Varvara-Kravtsova <varvara.kravczova2002@gmail.com>
2022-06-21 22:34:17 +0300

    feat(searchBooks): add subtitle to search results

--- src/pages/SearchBooks.tsx
+++ src/pages/SearchBooks.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { BookList } from "../components/BookList/BookList";
 import BackButton from "../components/BackButton/BackButton";
@@ -7,20 +7,32 @@ import { Pagination } from "../components/Pagination/Pagination";
 import { useAppSelector, useAppDispatch } from "../store/hooks/hooks";
 import { getBooks } from "../store/selectors/bookSelectors";
 import { fetchBooksBySearch } from "../store/slices/bookSlice";
+import { ISearchBooksApi } from "../types";
+import { bookApi } from "../services/bookService";
+import { Subtitle } from "../components/Subtitle/Subtitle";
 
 export const SearchBooks = () => {
  const { title = "", page = "" } = useParams();
  const { books } = useAppSelector(getBooks);
- const dispatch = useAppDispatch();
+ //  const dispatch = useAppDispatch();
+
+ const [searchResult, setSearchResult] = useState<ISearchBooksApi>();
+
+ //  useEffect(() => {
+ //   dispatch(fetchBooksBySearch({ title, page }));
+ //  }, [title, dispatch, page]);
 
  useEffect(() => {
-  dispatch(fetchBooksBySearch({ title, page }));
- }, [title, dispatch, page]);
+  bookApi.getBooksBySearch(title, page).then((books) => {
+   setSearchResult(books);
+  });
+ }, [title, page]);
 
  return (
   <>
    <BackButton />
-   <Title> '{title}' SEARCH RESULTS</Title>
+   <Title> '{title}' search results</Title>
+   <Subtitle>Found {searchResult?.total} books</Subtitle>
    <BookList books={books ? books : []} />
    <Pagination />
   </>
