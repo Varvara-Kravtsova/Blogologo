36e8623a5517817c68efab8217ed00ad9e9d8c1a
Varvara-Kravtsova <varvara.kravczova2002@gmail.com>
2022-06-21 12:06:17 +0300

    feat(pages): add cart page

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import MainTemplate from "../components/MainTemplate/MainTemplate";
 import { Account } from "../pages/Account";
+import { CartBooks } from "../pages/CartBooks";
 import { DetailsBook } from "../pages/DetailsBook";
 import { FavoritesBooks } from "../pages/FavoritesBooks";
 import { Home } from "../pages/Home";
@@ -17,6 +18,7 @@ export const AppRouter = () => {
     <Route path={routes.DETAILS_BOOK} element={<DetailsBook />} />
     <Route path={routes.SEARCH} element={<SearchBooks />} />
     <Route path={routes.FAVORITES} element={<FavoritesBooks />} />
+    <Route path={routes.CART} element={<CartBooks />} />
     <Route path={routes.SIGN_IN} element={<SignIn />} />
     <Route path={routes.SIGN_UP} element={<SignUp />} />
     <Route path={routes.ACCOUNT} element={<Account />} />
