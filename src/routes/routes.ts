662a8cd59f956c445d5ac30b07f780e6cc623094
Varvara-Kravtsova <varvara.kravczova2002@gmail.com>
2022-06-21 22:35:10 +0300

    feat(routes): change home route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,7 +2,7 @@ export enum routes {
  SIGN_IN = "/bookstore/sign-in",
  SIGN_UP = "/bookstore/sign-up",
  NOT_FOUND = "*",
- HOME = "/",
+ HOME = "/bookstore",
  DETAILS_BOOK = "/bookstore/books/:id",
  SEARCH = "/bookstore/search/:title/:page",
  FAVORITES = "/bookstore/favorites",
