cb3740146127a0e56115f27af7a32cb46d31fa05
Varvara-Kravtsova <varvara.kravczova2002@gmail.com>
2022-06-26 22:40:02 +0300

    feat(notFound): add backButton

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,9 +1,11 @@
+import BackButton from "../../components/BackButton/BackButton";
 import { Title } from "../../components/Title/Title";
 import { Container } from "./styles";
 
 export const NotFound = () => {
  return (
   <Container>
+   <BackButton />
    <Title>Page Not Found</Title>
   </Container>
  );
